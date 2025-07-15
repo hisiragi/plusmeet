7e8e62cce23aba48d5d614d1fb1b60aa3ef17464
hisiragi/ひしらぎ <127497658+hisiragi@users.noreply.github.com>
2025-07-15 11:08:22 +0900

    　

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
     window.addEventListener("load", () => {
       document.querySelector(".fade-in-text").classList.add("show");
       setTimeout(() => {
-  document.querySelector('.fade-in-text').style.fontSize = '100px';
-}, 3000);
-    });
